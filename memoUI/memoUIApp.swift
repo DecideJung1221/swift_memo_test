d9b2a2b421f35cec26a07be2837b37fc1c646358
Lucia <jung991221@naver.com>
2024-03-23 17:59:09 +0900

    memo list view

--- memoUI/memoUIApp.swift
+++ memoUI/memoUIApp.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 @main
 struct memoUIApp: App {
+    //메모 저장소를 속성으로 먼저 저장
+    @StateObject var store = MemoStore()
+    
+    
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
